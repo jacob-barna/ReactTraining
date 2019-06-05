82006e66677cab19e56659016e96c2491b83b8a2
Jacob Barna <Jacob.Barna@kiewit.com>
2019-06-05 14:31:35 -0500

    lift state to app & make courses presentation only

--- kiewit-react/src/App.js
+++ kiewit-react/src/App.js
@@ -1,17 +1,60 @@
-import React from "react";
+import React, { useState } from "react";
 import Home from "./Home";
 import Nav from "./Nav";
 import Courses from "./Courses";
 import { Route } from "react-router-dom";
 import ManageCourse from "./ManageCourse";
+import * as api from "./api/courseApi";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const App = () => {
+  //we can send a list of courses and a setter to the child components
+  const [courses, setCourses] = useState([]);
+
+  function loadCourses() {
+    api
+      .getCourses()
+      .then(courses => setCourses(courses))
+      .catch(() => {
+        toast.error("🦄🦄 Something bad happened 🦄🦄");
+      });
+  }
+
+  async function deleteCourse(id) {
+    try {
+      await api.deleteCourse(id);
+
+      //this.setState({
+      setCourses(courses.filter(course => course.id !== id));
+      toast.success("Delete successful");
+      // });
+    } catch (error) {
+      toast.error("🦄🦄 Something bad happened 🦄🦄");
+    }
+  }
+
   return (
     <>
+      <ToastContainer />
       <Nav />
       <Route path="/" component={Home} exact />
-      <Route path="/courses" component={Courses} />
-      <Route path="/course" component={ManageCourse} />
+      <Route
+        path="/courses"
+        render={props => (
+          <Courses
+            loadCourses={loadCourses}
+            deleteCourse={deleteCourse}
+            courses={courses}
+          />
+        )}
+      />
+      <Route
+        path="/course"
+        render={props => (
+          <ManageCourse loadCourses={loadCourses} courses={courses} />
+        )}
+      />
     </>
   );
 };
--- kiewit-react/src/Courses.js
+++ kiewit-react/src/Courses.js
@@ -1,60 +1,18 @@
 import React from "react";
-import * as api from "./api/courseApi";
-import { ToastContainer, toast } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
+
 import { Link } from "react-router-dom";
 
 //function component - React components should start with a capital letter
 //this is a convention of the framework - it looks for components when caps used
 class Courses extends React.Component {
-  //experimental - can use with babel until part of ecmascript
-  // this can replace the constructor
-  // state = {
-  //   courses: [
-  //     {id: 1, title: 'clean'} ///etc...
-  //   ]
-  // };
-
-  constructor(props) {
-    //props -> like args
-    super(props);
-
-    this.state = {
-      courses: []
-    };
-  }
-
   componentDidMount() {
-    api
-      .getCourses()
-      .then(courses => this.setState({ courses }))
-      .catch(() => {
-        toast.error("🦄🦄 Something bad happened 🦄🦄");
-      });
-    //setState({courses: api.getCourses()})
-  }
-
-  async deleteCourse(id) {
-    try {
-      await api.deleteCourse(id);
-
-      //      .then(() => {
-      this.setState({
-        courses: this.state.courses.filter(course => course.id !== id)
-      });
-    } catch (error) {
-      toast.error("🦄🦄 Something bad happened 🦄🦄");
-    }
-
-    //    })
-    //.catch(() => toast.error("🦄🦄 Something bad happened 🦄🦄")); //.then(response => alert(response));
+    this.props.loadCourses();
   }
 
   render() {
     return (
       <>
         <h1>Courses</h1>
-        <ToastContainer />
         <table className="table">
           <thead>
             <tr>
@@ -65,10 +23,10 @@ class Courses extends React.Component {
             </tr>
           </thead>
           <tbody>
-            {this.state.courses.map(c => (
+            {this.props.courses.map(c => (
               <tr key={c.id}>
                 <td>
-                  <button onClick={() => this.deleteCourse(c.id)}>
+                  <button onClick={() => this.props.deleteCourse(c.id)}>
                     Delete
                   </button>
                 </td>
